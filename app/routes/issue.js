fa0c4eef8a6574b88ba40040d3405e53d7307543
Christian Jonassen <flyrev@gmail.com>
2016-07-05 11:31:40 +0200

    Issue route

--- app/routes/issue.js
+++ app/routes/issue.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    setupController: function(controller, model) {
+        controller.set('model', model);
+        controller.set('user', this.store.createRecord('user', {name: "User_" + new Date().getTime()}));
+        controller.set('users', this.store.findAll('user'));
+        controller.set('artists', this.store.findAll('artist'));
+    },
+
+    init() {
+        this.store.createRecord('user', {name: "flyrev"});
+        this.store.createRecord('artist', {name: "Angus Young"});
+        this.store.createRecord('artist', {name: "Brian Johnson"});
+    },
+
+    actions: {
+        setUser(user) {
+	    this.controller.set('user', user);
+	},
+
+        submit(model) {
+	    model.save();
+	}
+    }
 });
